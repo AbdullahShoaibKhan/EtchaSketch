30c7407ace0be478adc96255d627b251aca4ef28
AbdullahShoaib <abdullahshoaib0333@gmail.com>
2022-07-16 22:35:52 +0500

    final commit

--- script.js
+++ script.js
@@ -2,17 +2,21 @@
     let content=document.querySelector('content');
     let black=document.querySelector('#black');
     let rainbow=document.querySelector('#rainbow');
-    let div;
-    let allDivs;
+    let div, allDivs;
     size=prompt('enter number');
-    size=parseInt(size);
-    console.log(size); 
+    validate(size);
 
-    boxs=Math.pow(size,2);
-    
-    make(boxs);
-    newsize(size);
-    
+    function validate(size){
+    size=parseInt(size);
+    if(size>10){
+        alert("Enter a Number Smaller than Or equal to 10")
+        size=prompt('enter number');
+    }else{
+        boxs=Math.pow(size,2);
+        make(boxs);
+        newsize(size);
+    }
+    } 
     rainbow.addEventListener('click',mixclr)
     black.addEventListener('click',dark)
 
@@ -24,21 +28,25 @@
             }
         }
     function dark(){
-        allDivs.forEach(element=>{
-        element.style.backgroundColor='white';    
-        });
+        allDivs=container.querySelectorAll('div');
+        allDivs.forEach(element => {
+            element.addEventListener('mouseover',(e)=>{
+                    options=['#353935','#28282B','#1B1212',
+                    '#36454F','#000000','#343434','#9E9E9E',
+                    '#757575','#424242'];
+                    e.target.style.backgroundColor=options[~~(Math.random()*options.length)];})    
+                });
+            
         }
     function mixclr(){
         allDivs=container.querySelectorAll('div');
         allDivs.forEach(element => {
             element.addEventListener('mouseover',(e)=>{
-                console.log('dada');
                 let ranR =Math.floor(Math.random()*256);
                 let ranG =Math.floor(Math.random()*256);
                 let ranB =Math.floor(Math.random()*256);
             e.target.style.backgroundColor=`rgb(${ranR},${ranG},${ranB})`;})    
-        });
-        
+        });  
         }
     function newsize(size){
         container.style.gridTemplateColumns=`repeat(${size},1fr)`    
